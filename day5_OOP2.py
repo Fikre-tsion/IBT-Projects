fb2762dc1970813d6b62df8ad81657d1ee2afd89
Fikire-Getnet <223140589+Fikre-tsion@users.noreply.github.com>
2026-07-14 08:31:50 +0300

    Delete day5_OOP2.py

--- day5_OOP2.py
+++
@@ -1,5 +0,0 @@
-# Deep down to OOP principles, like Inheritance,...
-# Inheritance: for reusability of attributes by child from parent
-# "is a" relation and avoid copy-pasting same shit
-
-#For the project: 1. saving and current accounts
